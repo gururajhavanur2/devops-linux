0cc3cf92a0c0e4cc0945068bbc791d29c0ff117b
gururajhavanur2 <124485675+gururajhavanur2@users.noreply.github.com>
2023-02-05 11:38:56 +0530

    Update install-tomcat.sh

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "-------------------------------update ubuntu-------------------------"
 sudo apt update
+echo "---------------------------------installing jdk--------------------------"
 sudo apt install -y openjdk-11-jdk
+echo"--------------------------------installing tomcat9-----------------------------"
 sudo apt install -y tomcat9
